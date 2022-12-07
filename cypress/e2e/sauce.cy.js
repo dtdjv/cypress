0b95b576caeeada864a43b063353b1122fbe64de
dtdjv <98287907+dtdjv@users.noreply.github.com>
2022-12-07 20:32:05 +0100

    added sortList function with written test

--- cypress/e2e/sauce.cy.js
+++ cypress/e2e/sauce.cy.js
@@ -10,6 +10,12 @@ describe('Standard user test cases.', () => {
         })
         cy.visit('https://www.saucedemo.com/')
     })
+
+    function sortList (sortBy) {
+
+        cy.get('[data-test="product_sort_container"]').select(sortBy)
+        cy.get('[data-test="product_sort_container"]').should('contain.text', sortBy)
+    }
     
     it('User can access login page', function () {
 
@@ -25,7 +31,7 @@ describe('Standard user test cases.', () => {
         cy.login(fix.normalUser.login, fix.normalUser.password)
         cy.get('.title').should('contain.text', itemToCheck)    
      })
-     
+
      it('User can access hamburger menu.', function () {
 
         const menuItems = ['All Items', 'About', 'Logout', 'Reset App State']
@@ -33,7 +39,35 @@ describe('Standard user test cases.', () => {
         cy.login(fix.normalUser.login, fix.normalUser.password)
         cy.get('#react-burger-menu-btn').should('be.visible').click()
         cy.wait(1000)
-        cy.get('nav.bm-item-list').should('be.visible').should('contain.text', menuItems)
+        cy.get('nav.bm-item-list').should('be.visible').children().then((children) => {
+            let i = 0            
+            // cy.log(children.length) for debug purposes
+            while (i < children.length) {                
+                cy.get(children[i]).should('contain.text', menuItems[i])
+                i++                
+              } 
+        })
      })
 
+     it('User can access product cart.', function () { 
+
+        const itemToCheck = 'Your Cart'
+
+        cy.login(fix.normalUser.login, fix.normalUser.password)
+        cy.get('.shopping_cart_link').click()
+        cy.get('.title').should('contain.text', itemToCheck)   
+    })
+
+     it('User can sort product list', function () { 
+
+        const sortByItems = ['Name (A to Z)', 'Name (Z to A)', 'Price (low to high)', 'Price (high to low)']
+        
+        cy.login(fix.normalUser.login, fix.normalUser.password)
+        let i = 0
+        while (i < sortByItems.length) {
+            sortList(sortByItems[i])
+            i++
+        }
+     })
+     
 })
