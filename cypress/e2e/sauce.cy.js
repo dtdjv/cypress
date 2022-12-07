7ed36c4e17b95a1d207571f65e9cb871af258ad1
dtdjv <98287907+dtdjv@users.noreply.github.com>
2022-12-07 20:06:39 +0100

    added command

--- cypress/e2e/sauce.cy.js
+++ cypress/e2e/sauce.cy.js
@@ -1,30 +1,39 @@
 /// <reference types="cypress" />
 
-
-let fix
-beforeEach(function ()  {         
-     
-    cy.fixture('sauce').then(function (sauce)  {
-        fix = sauce        
-    })    
-    cy.visit('https://www.saucedemo.com/')
-})
-
 describe('Standard user test cases.', () => {   
+
+    let fix
+    beforeEach(function ()  {         
+     
+        cy.fixture('sauce').then(function (sauce)  {
+            fix = sauce        
+        })
+        cy.visit('https://www.saucedemo.com/')
+    })
     
     it('User can access login page', function () {
 
     cy.get('[data-test="username"]').should('be.visible')
     cy.get('[data-test="password"]').should('be.visible')
     cy.get('[data-test="login-button"]').should('be.visible')
-
     })
 
     it('User can login.', function () {
 
-        cy.get('[data-test="username"]').type(fix.normalUser.login)
-        cy.get('[data-test="password"]').type(fix.normalUser.password)
-        cy.get('[data-test="login-button"]')
-    
+        const itemToCheck = 'Products'
+
+        cy.login(fix.normalUser.login, fix.normalUser.password)
+        cy.get('.title').should('contain.text', itemToCheck)    
      })
+     
+     it('User can access hamburger menu.', function () {
+
+        const menuItems = ['All Items', 'About', 'Logout', 'Reset App State']
+
+        cy.login(fix.normalUser.login, fix.normalUser.password)
+        cy.get('#react-burger-menu-btn').should('be.visible').click()
+        cy.wait(1000)
+        cy.get('nav.bm-item-list').should('be.visible').should('contain.text', menuItems)
+     })
+
 })
