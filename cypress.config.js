6f7c275e40803dc4884bc455a291e5474ba16cff
dtdjv <dawid-szczepaniak@windowslive.com>
2023-03-04 11:05:30 +0100

    working

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,24 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
   e2e: {
     "chromeWebSecurity": false,
     "experimentalSessionAndOrigin": true,
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+        });
+        
+        on("file:preprocessor", bundler);
+        await addCucumberPreprocessorPlugin(on, config);
+        
+        return config;
       // implement node event listeners here
       
     },
+    specPattern: "cypress/e2e/*/.feature",
   },
 });
